9c751c8ad343019b00e45e89fe8015e6a63cc65d
4ppolyon <romain.alves@etu.univ-grenoble-alpes.fr>
2023-09-18 14:43:32 +0200

    Ajout TexteGauche et TestTexte

---
+++ src/Documents_et_alignement/TexteGauche.java
@@ -0,0 +1,11 @@
+package Documents_et_alignement;
+
+public class TexteGauche extends TexteCentre{
+    public TexteGauche(String s){
+        super(s);
+    }
+    @Override
+    public String toString() {
+        return texte;
+    }
+}
