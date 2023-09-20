bc69d2c53ae147658e0a19dba8400b7bfb5684e0
4ppolyon <romain.alves@etu.univ-grenoble-alpes.fr>
2023-09-20 10:31:41 +0200

    Passage à la partie 3

--- src/Documents_et_alignement/TestTexte.java
+++ src/Documents_et_alignement/TestTexte.java
@@ -1,10 +1,19 @@
 package Documents_et_alignement;
 
 public class TestTexte {
-    public static void main(String[] args){
-        TexteCentre texte = new TexteCentre("Hello World!");
-        texte.fixeLargeur(80);
-        String t2 = texte.toString();
+    public static void main(String[] args) {
+        String t = "Hello World!";
+        TexteGauche texte1 = new TexteGauche(t);
+        TexteCentre texte2 = new TexteCentre(t);
+        TexteDroite texte3 = new TexteDroite(t);
+        texte1.fixeLargeur(80);
+        texte2.fixeLargeur(80);
+        texte3.fixeLargeur(80);
+        String t1 = texte1.toString();
+        String t2 = texte2.toString();
+        String t3 = texte3.toString();
+        System.out.println(t1);
         System.out.println(t2);
+        System.out.println(t3);
     }
 }
