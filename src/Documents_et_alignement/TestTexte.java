88f912de3b68041fa35e9732e29a8963d4e32987
4ppolyon <romain.alves@etu.univ-grenoble-alpes.fr>
2023-09-20 10:02:29 +0200

    petite modif dans TexteCentre

--- src/Documents_et_alignement/TestTexte.java
+++ src/Documents_et_alignement/TestTexte.java
@@ -2,7 +2,7 @@ package Documents_et_alignement;
 
 public class TestTexte {
     public static void main(String[] args){
-        TexteDroite texte = new TexteDroite("Hello World!");
+        TexteCentre texte = new TexteCentre("Hello World!");
         texte.fixeLargeur(80);
         String t2 = texte.toString();
         System.out.println(t2);
